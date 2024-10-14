2e2c84be8fd63c0374ac12c1b6b6267d722a8374
Apurbo dey <apurbomishu24@gmail.com>
2024-10-14 15:36:51 -0400

    Updated the files

--- librarysystem.py
+++ librarysystem.py
@@ -1,51 +1,9 @@
-import urllib.request, urllib.parse, json, re, math
+#Buying a book from a library using web scraping-
+import urllib.request, urllib.parse, json, re
 from bs4 import BeautifulSoup
 
 html=urllib.request.urlopen('https://books.toscrape.com/')
 soup=BeautifulSoup(html,'html.parser')
-# print(soup.prettify())
-# tags=soup.find('ul',class_="nav nav-list")
-# lsttag=tags.find_all('li')
-# for tag in lsttag:
-#     t=tag.find('a')
-#     print(t.string.strip())
-#     print(t.get('href',None))
-
-#Get all the books information-
-# tags=soup.find_all('article',class_="product_pod")
-# for tag in tags:
-#     t=tag.find('h3')
-#     p=tag.find('p',class_="price_color")
-#     star=tag.find('p',class_="star-rating")
-#     availability=tag.find('p',class_="instock availability")
-#     # print(t.string)
-#     a=t.find('a')
-#     print(f"Book Name: {a.get('title',None)}")
-#     print(f"Price: {p.text}")
-#     print(f"Rate:{star.get("class",None)[1]} Stars")
-#     print(f"Availability:{availability.text.strip()}")
-
-# print(lsttag[2])
-
-# t=lsttag[1].find('a')
-# print(t.string.strip())
-# print(t.get('href',None))
-
-#Searching link sorting by name-
-# bookname=input('Search for the genre: ')
-# tags=soup.find('ul',class_="nav nav-list")
-# lsttag=tags.find_all('li')
-# found=False
-# for tag in lsttag:
-#     t=tag.find('a')
-#     if bookname.capitalize()==t.string.strip():
-#         print(t.get('href',None))
-#         found=True
-
-# if not found:
-#     print("Not found!")
-
-#Searching for a book and it's price and availability by it's genre-
 bookname=input('Search for the genre: ')
 tags=soup.find('ul',class_="nav nav-list")
 lsttag=tags.find_all('li')
@@ -103,7 +61,7 @@ for tag in seltags:
                         if user_infos['username']==username.lower() and user_infos['email']==email:
                             if user_infos['balance']>=float(bookPrice):
                                 print("Your purchase has been completed!")
-                                user_infos['balance']-=math.ceil(float(bookPrice))
+                                user_infos['balance']-=float(bookPrice)
                         
                             elif  user_infos['balance']<float(bookPrice):
                                 print("Insufficient balance in your account!")
